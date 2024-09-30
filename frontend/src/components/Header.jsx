b4a7993039d73fe41665a505748b58ee8cc1ccc0
sarathsathish07 <sarathsathish1998@gmail.com>
2024-09-30 18:48:43 +0530

    title

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-          <Navbar.Brand> REACT</Navbar.Brand>
+          <Navbar.Brand>URL SHORETENER</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
